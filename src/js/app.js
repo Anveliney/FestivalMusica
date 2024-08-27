25d02b7bf8c257a0b1ea4e7ae5c1d8a6fc924ea3
Anveliney <calvilloortegapabloalejandro@gmail.com>
2024-08-26 22:01:53 -0600

    Progreso

--- src/js/app.js
+++ src/js/app.js
@@ -4,11 +4,13 @@ document.addEventListener('DOMContentLoaded', function(){
 
 function crearGaleria(){
     const galeria = document.querySelector('.galeria-imagenes');
+    const CANTIDADDEIMAGENES = 16;
 
-    for(let i = 1; i <= 16; i++){
+    for(let i = 1; i <= CANTIDADDEIMAGENES; i++){
         const imagen = document.createElement('IMG');
         imagen.src = `src/img/gallery/full/${i}.jpg`
         imagen.alt = `Imagen Galeria`
+        imagen.class = ``
 
         galeria.appendChild(imagen);
 
